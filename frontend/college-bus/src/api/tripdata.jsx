fc4720ef9e5fbd120b7c7b3db92865c959d7b6dc
cloudkranthi <shivakranthi0006@gmail.com>
2026-02-08 16:28:34 +0530

    exported correctly tripdata

--- frontend/college-bus/src/api/tripdata.jsx
+++ frontend/college-bus/src/api/tripdata.jsx
@@ -1,6 +1,7 @@
 import API from'./axios'
-export const tripdata=async (cursor=null)=>{
+ const tripdata=async (cursor=null)=>{
     const path=cursor?`&cursor=${cursor}`:'';
     const res = await  API.post(`/trip/trips?limit=10${path}`)
     return res.data;
 }
+export default tripdata;
